ef2edd71536d4c4bf59019c54020df9c754cd0ec
nino bagus <ninobagus.mail@gmail.com>
2021-03-17 11:25:01 +0700

    adding meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="/css/app.css">
     <script src="/js/app.js "></script>
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>AMR Task </title>
     <style>
 .center {
